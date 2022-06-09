0a5b959ad4a836e860e82fd6f976a4e7eb6eb985
Matteo <89725278+MattFus@users.noreply.github.com>
2022-06-09 10:50:45 -0400

    Add files via upload

--- NetworkProject/F1/firewall.sh
+++ NetworkProject/F1/firewall.sh
@@ -10,37 +10,58 @@ iptables -P OUTPUT DROP
 iptables -P FORWARD DROP
 
 #Create custom chains to handle network traffic
-    #TAP TO DMZ
-iptables -N tapDmz
-iptables -N dmzTap
-
-    #GREEN TO DMZ
-iptables -N greenDmz
-iptables -N dmzGreen
+    #RED TO GREEN
+iptables -N redGreen
+iptables -N greenRed
 
     #RED TO DMZ
 iptables -N redDmz
 iptables -N dmzRed
 
+    #INET TO GREEN
+iptables -N inetGreen
+iptables -N greenInet
+
+    #INET TO DMZ
+iptables -N inetDmz
+ipatbles -N dmzInet
+
 #Add our custom chains to the forward chain
-iptables -A FORWARD -i eth0 -s 172.16.8.76/30 -d 172.16.0.0/22 -j tapDmz
-iptables -A FORWARD -o eth0 -s 172.16.0.0/22 -d 172.16.8.76/30 -j dmzTap
+iptables -A FORWARD -i eth1 -d 172.16.4.0/22 -j redGreen
+iptables -A FORWARD -o eth1 -s 172.16.4.0/22 -j greenRed
 
-iptables -A FORWARD -i eth0 -s 172.16.4.0/22 -d 172.16.0.0/22 -j greenDmz
-iptables -A FORWARD -o eth0 -s 172.16.0.0/22 -d 172.16.4.0/22 -j dmzGreen
+iptables -A FORWARD -i eth1 -d 172.16.0.0/22 -j redDmz
+iptables -A FORWARD -o eth1 -s 172.16.0.0/22 -j dmzRed
 
-iptables -A FORWARD -i eth0 -s 172.16.8.0/26 -d 172.16.0.0/22 -j redDmz
-iptables -A FORWARD -o eth0 -s 172.16.0.0/22 -d 172.16.8.0/26 -j dmzRed 
+iptables -A FORWARD -i eth0 -d 172.16.4.0/22 -j inetGreen
+iptables -A FORWARD -o eth0 -s 172.16.4.0/22 -j greenInet
 
-#CHAIN RULES
-    ##TAP_DMZ##
-iptables -A tapDmz -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
-iptables -A dmzTap -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -A FORWARD -i eth0 -o eth2 -d 172.16.0.0 -p tcp --dport 80 -j inetDmz #HTTP
+iptables -A FORWARD -i eth0 -o eth2 -d 172.16.0.0 -p tcp --dport 25 -j inetDmz #SMTP
+iptables -A FORWARD -i eth0 -o eth2 -d 172.16.0.0 -p tcp --dport 23 -j inetDmz #Telnet
+iptables -A FORWARD -i eth0 -o eth2 -d 172.16.0.0 -p tcp --dport 21 -j inetDmz #FTP
+iptables -A FORWARD -i eth0 -o eth2 -d 172.16.0.0 --dport 53 -j inetDmz #DNS
+iptables -A FORWARD -i eth2 -o eth0 -s 172.16.0.0 -j dmzInet
 
-    ##GREEN_DMZ##
-iptables -A greenDmz -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
-iptables -A dmzGreen -m state --state ESTABLISHED,RELATED -j ACCEPT
+    #CHAIN RULES
+    ##RED_GREEN##
+iptables -A redGreen -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -A GreenRed -m state --state NEW,ESTABLISHED,RELATED  -j ACCEPT 
 
     ##RED_DMZ##
 iptables -A redDmz -m state --state ESTABLISHED,RELATED -j ACCEPT
-iptables -A dmzRed -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
+iptables -A dmzRed -m state --state NEW,ESTABLISHED,RELATED  -j ACCEPT
+
+    ##INET_GREEN##
+iptables -A greenInet -m state --state NEW,ESTABLISHED,RELATE -j ACCEPT
+iptables -A inetGreen -m state --state ESTABLISHED,RELATE -j ACCEPT
+
+    ##INET_DMZ##
+iptables -A inetDmz -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
+iptables -A dmzInet -m state --state ESTABLISHED,RELATED -j ACCEPT
+
+    #NATTING
+iptables -t nat -A PREROUTING -i eth0 --dport 80 -j DNAT --to-destination 172.16.0.2
+iptables -t nat -A PREROUTING -i eth0 --dport 25 -j DNAT --to-destination 172.16.0.3
+iptables -t nat -A PREROUTING -i eth0 --dport 23 -j DNAT --to-destination 172.16.2.3
+iptables -t nat -A PREROUTING -i eth0 --dport 21 -j DNAT --to-destination 172.16.2.2
